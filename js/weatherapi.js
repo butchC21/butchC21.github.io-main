5031fc65fc60a93f9edf83cf58e3347bb7741466
butchC21 <76933472+butchC21@users.noreply.github.com>
2021-07-20 22:47:07 +0800

    Delete js directory

--- js/weatherapi.js
+++
@@ -1,9 +0,0 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=metric&appid=f041292742df908f0af429eacd66c2b8";
-fetch(apiURL)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
-    
-   
-  });
